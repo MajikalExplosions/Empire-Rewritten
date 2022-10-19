85ba18b68b935103810203f324c34581cd0c89f6
MajikalExplosions <31389551+MajikalExplosions@users.noreply.github.com>
2022-10-18 17:12:33 -0700

    Add Emblem code
    Still a draft; textures and texture loading will probably be redone. 512x512 textures are too large; emblem texture generation should be on a thread.

---
+++ Source/1.3/Emblems/Emblem.cs
@@ -0,0 +1,194 @@
+﻿using JetBrains.Annotations;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+using UnityEngine;
+using UnityEngine.UI;
+using Verse;
+
+namespace Empire_Rewritten.Emblems
+{
+    public class Emblem
+    {
+        private Color iconColor, backColor, accentColor;
+        private EmblemPartDef iconPart, patternPart, shapePart;
+        
+        public Color IconColor
+        {
+            get => iconColor;
+            set
+            {
+                if (iconColor != null && iconColor == value) return;
+                ChangedSinceGeneration = true;
+                iconColor = value;
+            }
+        }
+        
+        public Color BackColor
+        {
+            get { return backColor; }
+            set
+            {
+                if (backColor != null && backColor == value) return;
+                ChangedSinceGeneration = true;
+                backColor = value;
+            }
+        }
+
+        public Color AccentColor
+        {
+            get { return accentColor; }
+            set
+            {
+                if (accentColor != null && accentColor == value) return;
+                ChangedSinceGeneration = true;
+                accentColor = value;
+            }
+        }
+        
+        public EmblemPartDef Icon
+        {
+            get { return iconPart; }
+            set
+            {
+                if (iconPart != null && iconPart == value) return;
+                ChangedSinceGeneration = true;
+                iconPart = value;
+            }
+        }
+
+        public EmblemPartDef Pattern
+        {
+            get { return patternPart; }
+            set
+            {
+                if (patternPart != null && patternPart == value) return;
+                ChangedSinceGeneration = true;
+                patternPart = value;
+            }
+        }
+
+        public EmblemPartDef Shape
+        {
+            get { return shapePart; }
+            set
+            {
+                if (shapePart != null && shapePart == value) return;
+                ChangedSinceGeneration = true;
+                shapePart = value;
+            }
+        }
+
+        private bool ChangedSinceGeneration;
+        private Texture2D Texture;
+
+        public Emblem(Color iconColor, Color backColor, Color accentColor, EmblemPartDef icon, EmblemPartDef pattern, EmblemPartDef shape)
+        {
+            ChangedSinceGeneration = true;
+            IconColor = iconColor;
+            BackColor = backColor;
+            AccentColor = accentColor;
+            Icon = icon;
+            Pattern = pattern;
+            Shape = shape;
+        }
+
+        public Emblem() : this(PresetColors[0, 0], PresetColors[0, 1], PresetColors[1, 0],
+            DefDatabase<EmblemPartDef>.GetNamed("icon_empty"), DefDatabase<EmblemPartDef>.GetNamed("pattern_empty"), DefDatabase<EmblemPartDef>.GetNamed("shape_empty")) { }
+
+        public Texture2D GetTexture()
+        {
+            if (ChangedSinceGeneration || Texture == null)
+            {
+                Texture = new Texture2D(TEXTURE_WIDTH, TEXTURE_HEIGHT);
+                if (! VerifyTextures())
+                {
+                    Utils.Logger.Error("Selected textures have different sizes!");
+                    return Texture;
+                }
+
+                Color[] icon = Icon.Texture.GetPixels();
+                Color[] pattern = Pattern.Texture.GetPixels();
+                Color[] shape = Shape.Texture.GetPixels();
+
+                if (icon.Length != pattern.Length || icon.Length != shape.Length || pattern.Length != shape.Length)
+                {
+                    Utils.Logger.Error("Selected textures have different sizes!");
+                    return Texture;
+                }
+
+                Color[] output = new Color[icon.Length];
+
+                for (int i = 0; i < output.Length; i++) {
+                    //Start with colored shape
+                    output[i] = shape[i] * BackColor;
+
+                    //Add together output/pattern, with pattern taking more the higher its albedo. Mask result with respect to shape.
+                    if (pattern[i].a != 0) output[i] = ScaleAlbedo(output[i], pattern[i] * AccentColor, mask: true);
+
+                    //Add icon on top, overwriting whatever's below it.
+                    if (icon[i].a != 0) output[i] = ScaleAlbedo(output[i], icon[i] * IconColor);
+                }
+                Texture.SetPixels(output);
+                Texture.Apply(true, true);
+
+            }
+            ChangedSinceGeneration = false;
+            return Texture;
+        }
+
+        private static Dictionary<EmblemPartCategory, List<EmblemPartDef>> _partCache = new Dictionary<EmblemPartCategory, List<EmblemPartDef>>();
+
+        public static List<EmblemPartDef> GetPartsFor(EmblemPartCategory category)
+        {
+            if (_partCache.ContainsKey(category)) return _partCache[category];
+            List<EmblemPartDef> validParts = new List<EmblemPartDef>();
+            foreach (EmblemPartDef part in DefDatabase<EmblemPartDef>.AllDefs)
+            {
+                if (part.category == category) validParts.Add(part);
+            }
+            _partCache[category] = validParts;
+            return _partCache[category];
+        }
+        private Color ScaleAlbedo(Color bot, Color top, bool mask = false)
+        {
+            Color c = bot * (1 - top.a) + top * top.a;
+            if (mask) c.a *= bot.a;
+            return c;
+        }
+        private bool VerifyTextures()
+        {
+            if (Icon.Texture.width != TEXTURE_WIDTH || Icon.Texture.height != TEXTURE_HEIGHT ||
+                Pattern.Texture.width != TEXTURE_WIDTH || Pattern.Texture.height != TEXTURE_HEIGHT ||
+                Shape.Texture.width != TEXTURE_WIDTH || Shape.Texture.height != TEXTURE_HEIGHT) return false;
+            return true;
+        }
+
+        /*
+         * First 3 rows mostly from Stellaris (flags/colors.txt, if you own the game. The "flag" colors, not "ship"):
+         * intense_red, orange, yellow, green
+         * intense_turquoise, intense_blue, indigo, intense_purple
+         * intense_pink, beige, dark_brown, <Rimworld UI Button Color>
+         * 
+         * Last row is:
+         * off-white, light gray, dark gray, off-black
+         */
+        public static readonly Color[,] PresetColors = {
+            { FromInt(241, 37, 15), FromInt(215, 100, 35), FromInt(204, 148, 41), FromInt(46, 102, 41) },
+            { FromInt(55, 178, 170), FromInt(30, 159, 220), FromInt(47, 19, 127), FromInt(139, 39, 184) },
+            { FromInt(176, 49, 128), FromInt(150, 126, 90), FromInt(58, 38, 23), FromInt(105, 81, 45) },
+            { FromInt(239, 239, 239), FromInt(153, 153, 153), FromInt(51, 51, 51), FromInt(16, 16, 16) }
+        };
+
+        private static Color FromInt(int r, int g, int b)
+        {
+            return new Color(r / 255f, g / 255f, b / 255f);
+        }
+
+        public const int TEXTURE_WIDTH = 512, TEXTURE_HEIGHT = 512;
+    }
+}
---
+++ Source/1.3/Utils/GUI/FlexRect/TestEmblemCreationWindow.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+using Empire_Rewritten.Emblems;
+using System.Net;
+using RimWorld;
+
+namespace Empire_Rewritten.Utils
+{
+    public class TestEmblemCreationWindow : Window
+    {
+        //TODO Do we want something like this?
+        public override Vector2 InitialSize => new Vector2(800, 600);
+        private const int CUSTOM_COLOR = 100;
+        private const int GRID_CELL_WIDTH = 80;
+        private FlexRect root;
+
+        private Emblem Emblem;
+        private bool IconPanelOpen, PatternPanelOpen, ShapePanelOpen;
+
+        public TestEmblemCreationWindow()
+        {
+            forcePause = true;
+            preventCameraMotion = true;
+            onlyOneOfTypeAllowed = true;
+            resizeable = true;
+            root = new FlexRect("root");
+            root.BottomLeft(0.45f, 0.8f, "preview");
+            root.TopLeft(0.45f, 0.2f, "headerBar");
+
+            FlexRect settings = root.Right(0.55f);
+            settings.BottomRight(0.5f, 0.1f, "save");
+            settings.BottomLeft(0.5f, 0.1f, "discard");
+
+            FlexRect pattern = settings.Top(0.9f).Top(0.5f, "patternPanel");
+            FlexRect color = settings.Top(0.9f).Bottom(0.5f, "colorPanel");
+            settings.Top(0.9f, "settingsPanel");
+
+            pattern.Grid(3, 1, 0, 0, "patternIcon");
+            pattern.Grid(3, 1, 1, 0, "patternBack");
+            pattern.Grid(3, 1, 2, 0, "patternShape");
+            color.Grid(3, 1, 0, 0, "colorIcon");
+            color.Grid(3, 1, 1, 0, "colorBack");
+            color.Grid(3, 1, 2, 0, "colorAccent");
+
+            root.MergeChildren();
+
+            //TODO Use an actual emblem, not a new one.
+            Emblem = new Emblem();
+            Emblem.Icon = DefDatabase<EmblemPartDef>.GetNamed("icon_cross");
+            Emblem.Pattern = DefDatabase<EmblemPartDef>.GetNamed("pattern_hstripe2");
+            Emblem.Shape = DefDatabase<EmblemPartDef>.GetNamed("shape_round");
+            IconPanelOpen = false;
+            PatternPanelOpen = false;
+            ShapePanelOpen = false;
+        }
+
+        public override void DoWindowContents(Rect inRect)
+        {
+            Dictionary<string, Rect> rects = root.Resolve(inRect);
+            WindowHelper.ResetTextAndColor();
+
+            Widgets.DrawBox(rects["preview"].ContractedBy(4f), thickness: 2);
+
+            bool save = Widgets.ButtonText(rects["save"].ContractedBy(4f), "Save Changes");
+            bool close = Widgets.ButtonText(rects["discard"].ContractedBy(4f), "Discard Changes");
+
+            Text.Anchor = TextAnchor.MiddleCenter;
+            Text.Font = GameFont.Medium;
+            Widgets.Label(rects["headerBar"], "EMPIRE NAME HERE");
+            Text.Anchor = TextAnchor.UpperLeft;
+
+            DrawPreview(rects);
+            if (!IconPanelOpen && !PatternPanelOpen && !ShapePanelOpen) DrawCustomization(rects);
+            else DrawIconSelection(rects);
+
+            WindowHelper.ResetTextAndColor();
+            if (save) this.Close();//TODO SET THE EMBLEM!
+            if (close) this.Close();
+        }
+
+        private void DrawPreview(Dictionary<string, Rect> rects)
+        {
+            Widgets.DrawTextureFitted(rects["preview"], Emblem.GetTexture(), 0.85f);
+        }
+
+        private void DrawCustomization(Dictionary<string, Rect> rects)
+        {
+
+            Text.Anchor = TextAnchor.MiddleCenter;
+            Text.Font = GameFont.Small;
+
+            Widgets.DrawBox(rects["patternPanel"].ContractedBy(4f), thickness: 2);
+            Widgets.DrawBox(rects["colorPanel"].ContractedBy(4f), thickness: 2);
+
+
+            float height = rects["colorIcon"].ContractedBy(8f).height;
+
+            //Labels
+            float labelHeight = 50f;
+            Widgets.Label(rects["patternIcon"].ContractedBy(4f).TopPartPixels(labelHeight), "Icon");
+            Widgets.Label(rects["patternBack"].ContractedBy(4f).TopPartPixels(labelHeight), "Background\nPattern");
+            Widgets.Label(rects["patternShape"].ContractedBy(4f).TopPartPixels(labelHeight), "Shape");
+
+            Widgets.Label(rects["colorIcon"].ContractedBy(4f).TopPartPixels(labelHeight), "Icon Color");
+            Widgets.Label(rects["colorBack"].ContractedBy(4f).TopPartPixels(labelHeight), "Background\nColor");
+            Widgets.Label(rects["colorAccent"].ContractedBy(4f).TopPartPixels(labelHeight), "Accent Color");
+
+            //Pattern
+
+            int pi = GetPartButton(rects["patternIcon"].ContractedBy(8f + 4f).BottomPartPixels(height - labelHeight), Emblem.Icon);
+            int pb = GetPartButton(rects["patternBack"].ContractedBy(8f + 4f).BottomPartPixels(height - labelHeight), Emblem.Pattern);
+            int ps = GetPartButton(rects["patternShape"].ContractedBy(8f + 4f).BottomPartPixels(height - labelHeight), Emblem.Shape);
+
+            //Color
+            int ic = GetColorPalette(rects["colorIcon"].ContractedBy(8f).BottomPartPixels(height - labelHeight), Emblem.IconColor);
+            int bc = GetColorPalette(rects["colorBack"].ContractedBy(8f).BottomPartPixels(height - labelHeight), Emblem.BackColor);
+            int ac = GetColorPalette(rects["colorAccent"].ContractedBy(8f).BottomPartPixels(height - labelHeight), Emblem.AccentColor);
+
+            if (ic == CUSTOM_COLOR) ;
+            else if (ic > -1) Emblem.IconColor = Emblem.PresetColors[ic / Emblem.PresetColors.GetLength(1), ic % Emblem.PresetColors.GetLength(1)];
+
+            if (bc == CUSTOM_COLOR) ;
+            else if (bc > -1) Emblem.BackColor = Emblem.PresetColors[bc / Emblem.PresetColors.GetLength(1), bc % Emblem.PresetColors.GetLength(1)];
+
+            if (ac == CUSTOM_COLOR) ;
+            else if (ac > -1) Emblem.AccentColor = Emblem.PresetColors[ac / Emblem.PresetColors.GetLength(1), ac % Emblem.PresetColors.GetLength(1)];
+
+            if (pi == 1) IconPanelOpen = true;
+            if (pb == 1) PatternPanelOpen = true;
+            if (ps == 1) ShapePanelOpen = true;
+        }
+
+        private void DrawIconSelection(Dictionary<string, Rect> rects)
+        {
+
+            //TODO Use a scroll box
+            Rect rect = rects["settingsPanel"];
+            if (IconPanelOpen)
+            {
+                EmblemPartDef part = GetIconPalette(rect, Emblem.Icon, EmblemPartCategory.Icon);
+                if (part != null)
+                {
+                    IconPanelOpen = false;
+                    Emblem.Icon = part;
+                }
+            }
+            else if (PatternPanelOpen)
+            {
+                EmblemPartDef part = GetIconPalette(rect, Emblem.Pattern, EmblemPartCategory.Pattern);
+                if (part != null)
+                {
+                    PatternPanelOpen = false;
+                    Emblem.Pattern = part;
+                }
+            }
+            else if (ShapePanelOpen)
+            {
+                EmblemPartDef part = GetIconPalette(rect, Emblem.Shape, EmblemPartCategory.Shape);
+                if (part != null)
+                {
+                    ShapePanelOpen = false;
+                    Emblem.Shape = part;
+                }
+            }
+        }
+
+        private int GetPartButton(Rect r, EmblemPartDef highlight)
+        {
+            Widgets.DrawBox(r);
+            Widgets.DrawHighlightIfMouseover(r);
+            Widgets.DrawTextureFitted(r, highlight.Texture, 0.75f);
+            return Widgets.ButtonInvisible(r) ? 1 : -1;
+        }
+
+        private int GetColorPalette(Rect rect, Color highlight)
+        {
+            //Layout
+            FlexRect root = new FlexRect();
+
+            for (int i = 0; i < Emblem.PresetColors.GetLength(0); i++)
+                for (int j = 0; j < Emblem.PresetColors.GetLength(1); j++)
+                    root.Grid(Emblem.PresetColors.GetLength(0), Emblem.PresetColors.GetLength(1) + 2, i, j, "grid_" + i.ToString() + "_" + j.ToString());
+
+            root.Bottom(2f / (Emblem.PresetColors.GetLength(1) + 2), "custom");
+
+            Dictionary<string, Rect> r = root.Resolve(rect);
+
+            //Draw & Input
+            int selected = -1;
+            for (int i = 0; i < Emblem.PresetColors.GetLength(0); i++)
+            {
+                for (int j = 0; j < Emblem.PresetColors.GetLength(1); j++)
+                {
+                    Rect colorButton = r["grid_" + i.ToString() + "_" + j.ToString()].ContractedBy(2f);
+                    Widgets.DrawBoxSolid(colorButton, Emblem.PresetColors[i, j]);
+                    Widgets.DrawHighlightIfMouseover(colorButton);
+                    if (highlight == Emblem.PresetColors[i, j]) Widgets.DrawBox(colorButton, 2);
+                    if (Widgets.ButtonInvisible(colorButton)) selected = i * Emblem.PresetColors.GetLength(1) + j;
+                }
+            }
+
+            //TODO Figure out custom color. Which color picker to use?
+            //if (Widgets.ButtonText(r["custom"].ContractedBy(2f), "CUSTOM COLOR")) selected = CUSTOM_COLOR;
+            return selected;
+        }
+
+        private EmblemPartDef GetIconPalette(Rect rect, EmblemPartDef highlight, EmblemPartCategory partCategory)
+        {
+            //TODO Use a scroll box
+            //Calculate grid properties
+            int gridWidth = Mathf.FloorToInt(rect.width / GRID_CELL_WIDTH);
+            int actualGridSize = Mathf.FloorToInt(rect.width / gridWidth);
+            int gridHeight = Mathf.FloorToInt(rect.height / actualGridSize);
+            int maxCount = gridWidth * gridHeight;
+
+            EmblemPartDef selected = null;
+
+            //Get all valid parts
+            List<EmblemPartDef> parts = Emblem.GetPartsFor(partCategory);
+            if (parts.Count > maxCount) Logger.Warn("Too many parts! Grid can't fit them all.");
+
+            //Draw
+            for (int row = 0; row < Math.Min(parts.Count / gridWidth + 1, gridHeight); row++)
+            {
+                //Up to gridWidth, unless we're out of parts to draw.
+                for (int col = 0; col < Math.Min(gridWidth, parts.Count - row * gridWidth); col++)
+                {
+                    Logger.Log(row + " " + col);
+                    Rect cell = (new Rect(rect.xMin + col * actualGridSize, rect.yMin + row * actualGridSize, actualGridSize, actualGridSize));
+                    cell = cell.ContractedBy(4f);
+                    EmblemPartDef part = parts[row * gridWidth + col];
+                    Widgets.DrawTextureFitted(cell, part.Texture, 0.8f);
+                    Widgets.DrawHighlightIfMouseover(cell);
+
+                    if (highlight == part) Widgets.DrawBox(cell, thickness: 4);
+                    else Widgets.DrawBox(cell);
+
+                    if (Widgets.ButtonInvisible(cell)) selected = part;
+                }
+            }
+
+            return selected;
+        }
+    }
+}
